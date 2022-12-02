9a34624f2afc6365d50be4f6a1f39e93b1c9f441
TrulyHecky <117442917+TrulyHecky@users.noreply.github.com>
2022-12-02 15:18:07 +0600

    Added new solution.

---
+++ С++/Algorithms/Easy/5. Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& arr) {
+        int ans=0;
+        for(int i=0;i<arr.size();i++)
+            if(arr[i][0]=='-'||arr[i][1]=='-')
+                ans--;
+            else
+                ans++;
+        return ans;
+    }
+};
